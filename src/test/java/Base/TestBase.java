03906ee60e33bdb8b2fbf7bd08ac64843d802253
chaitra <chaitra@LAPTOP-LU9FG0N5>
2021-05-07 19:41:10 -0400

    msg

--- src/test/java/Base/TestBase.java
+++ src/test/java/Base/TestBase.java
@@ -33,7 +33,7 @@ import utilities.TestUtil;
 public class TestBase {
 	
 	
-	//WebDriver 
+	//WebDriver......
 	//properties
 	//logs---Log4j dependency,Logs,Log4j.properties,Logger file
 	//reports
